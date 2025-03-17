e67bb0afee839c86957f423af22f7eff95c632c0
git80123 <linqinghai80@gmail.com>
2025-03-17 21:06:39 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
         /**
          * optional
          **/
-        short_name: "仙人掌主题",
+        short_name: "久木笔记",
         description: siteConfig.description,
         lang: siteConfig.lang,
         icon: "public/icon.svg", // the source for generating favicon & icons
